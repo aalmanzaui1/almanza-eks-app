3547b2b83c64d0e9ac73f7821d00620555f3af7e
Alvaro Almanza <alvaroalmanza@MacBook-Pro-de-Alvaro.local>
2021-02-10 23:04:34 +0100

    adding python app containers

---
+++ cn1/cnt1.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/',methods=['GET'])
+def get_ui():
+    return send_from_directory('ui', 'almanzacn1.html')
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p','--port', type=int, default=8080)
+    args = parser.parse_args()
+    port = args.port
+    app.run(host='0.0.0.0',port=port)
